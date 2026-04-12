584121cff87459e6aecd946e2a0390e9579fe6fc
csrama <ramadafer1@gmail.com>
2026-04-12 06:41:13 +0000

    Delete 0-add.py

--- 0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Module that contains a function to add two floats."""
-
-
-def add(a: float, b: float) -> float:
-    """Return the sum of two float numbers."""
-    return a + b
