787461745cd6b10ee334dd2eef13908521af0705
csrama <156553209+csrama@users.noreply.github.com>
2026-04-12 11:16:40 +0300

    Update 2-measure_runtime.py

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """Measure runtime of four async comprehensions."""
 
-import asyncio
 import time
+import asyncio
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Run async_comprehension 4 times in parallel and return total time."""
+    """Run async_comprehension 4 times in parallel and return execution time."""
     start = time.perf_counter()
 
     await asyncio.gather(
