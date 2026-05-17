df857f7f2528d7e116ced4d5b32905900b4411a0
Rama Alshehri <156553209+csrama@users.noreply.github.com>
2026-05-17 11:04:28 +0300

    Implement StudentsController for student data handling

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,46 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+  static getAllStudents(req, res) {
+    const database = process.argv[2];
+
+    readDatabase(database)
+      .then((fields) => {
+        let response = 'This is the list of our students';
+
+        const sortedFields = Object.keys(fields).sort((a, b) => (
+          a.toLowerCase().localeCompare(b.toLowerCase())
+        ));
+
+        sortedFields.forEach((field) => {
+          response += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+        });
+
+        res.status(200).send(response);
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(req, res) {
+    const { major } = req.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    const database = process.argv[2];
+
+    readDatabase(database)
+      .then((fields) => {
+        res.status(200).send(`List: ${fields[major].join(', ')}`);
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+}
+
+export default StudentsController;
